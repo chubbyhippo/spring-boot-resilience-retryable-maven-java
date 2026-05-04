02213d0be6d0ed1ed695ab265000c61296f9306f
chubbyhippo <mkmsacc@outlook.com>
2026-05-04 19:04:53 +0700

    Add HelloService class to the project

---
+++ src/main/java/io/github/chubbyhippo/demo/HelloService.java
@@ -0,0 +1,25 @@
+package io.github.chubbyhippo.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class HelloService {
+
+    private final RestClient restClient;
+
+    public HelloService(RestClient.Builder restClientBuilder,
+                        // must be here to make sure the value is injected during application startup
+                        @Value("${hello.service.url}") String url) {
+        this.restClient = restClientBuilder
+                .baseUrl(url)
+                .build();
+    }
+
+    HelloResponse getHello(HelloRequest request) {
+        return restClient.get().uri("/hello", request.name())
+                .retrieve()
+                .body(HelloResponse.class);
+    }
+}
