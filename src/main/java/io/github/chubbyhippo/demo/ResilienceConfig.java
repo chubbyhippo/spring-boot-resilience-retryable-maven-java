381d39ad9e4cd1cc6709b3adc19e7a9022c27922
chubbyhippo <mkmsacc@outlook.com>
2026-05-05 10:31:32 +0700

    Add `@Configuration` to `ResilienceConfig` for Spring context recognition

--- src/main/java/io/github/chubbyhippo/demo/ResilienceConfig.java
+++ src/main/java/io/github/chubbyhippo/demo/ResilienceConfig.java
@@ -1,7 +1,9 @@
 package io.github.chubbyhippo.demo;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.resilience.annotation.EnableResilientMethods;
 
+@Configuration
 @EnableResilientMethods
 public class ResilienceConfig {
 }
