69f29eeb1e276013d86117766c2532b4de518c5f
chubbyhippo <mkmsacc@outlook.com>
2026-05-05 10:54:29 +0700

    Remove unused imports from `HelloController`

--- src/main/java/io/github/chubbyhippo/demo/HelloController.java
+++ src/main/java/io/github/chubbyhippo/demo/HelloController.java
@@ -1,7 +1,5 @@
 package io.github.chubbyhippo.demo;
 
-import org.springframework.resilience.annotation.Retryable;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
