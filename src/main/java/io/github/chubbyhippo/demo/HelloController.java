11eb138ebf0a9863f4e0a7a728077cf19007956c
chubbyhippo <mkmsacc@outlook.com>
2026-05-05 10:00:25 +0700

    Add `/hello` endpoint to `HelloController` with dependency on `HelloService`

--- src/main/java/io/github/chubbyhippo/demo/HelloController.java
+++ src/main/java/io/github/chubbyhippo/demo/HelloController.java
@@ -1,4 +1,19 @@
 package io.github.chubbyhippo.demo;
 
+import org.springframework.resilience.annotation.Retryable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    HelloResponse getHello(HelloRequest request) {
+        return helloService.getHello(request);
+    }
 }
