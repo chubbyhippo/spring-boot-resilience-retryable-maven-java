3ea9c341bc82e35692028563f7be5e17a201cb53
chubbyhippo <mkmsacc@outlook.com>
2026-05-05 10:54:53 +0700

    Add test

--- src/test/java/io/github/chubbyhippo/demo/DemoApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/demo/DemoApplicationTests.java
@@ -1,13 +1,47 @@
 package io.github.chubbyhippo.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureDataSourceInitialization;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import tools.jackson.databind.ObjectMapper;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockitoSpyBean
+    private RemoteHelloService service;
+    @Autowired
+    private MockMvcTester mockMvcTester;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("test retry attempts")
+    void testRetryAttempts() {
+        var request = new HelloRequest("test");
+        mockMvcTester.post()
+                .uri("/hello")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+                .assertThat()
+                .hasStatus(HttpStatus.OK);
+        verify(service, times(4)).getHelloWithRetry(any());
+    }
+
 
 }
